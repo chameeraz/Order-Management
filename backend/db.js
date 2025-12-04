a9263a41352b62b3df0035f452150b517669ad79
Chameera Mayura <76671336+chameeraz@users.noreply.github.com>
2025-12-04 19:18:10 +0530

    bypass

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const DATABASE_URL = 'postgresql://neondb_owner:npg_CJBbUaI6i4NS@ep-solitary-dew-ah45qlvy-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require';
+process.env.DATABASE_URL = process.env.DATABASE_URL || DATABASE_URL;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
